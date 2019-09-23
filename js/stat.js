c12df3b5b654b5a6619a08a752703196ad74fb80
Venera <venera_husainova@mail.ru>
2019-09-23 10:44:27 +0300

    second stat.js

--- js/stat.js
+++ js/stat.js
@@ -4,7 +4,6 @@ var WIDTH_COLUMN = 40;
 var MAXHIEGHT_COLUMN = 150;
 var DISTANCE = 50;
 var TEXT_Y_NAME = 270;
-var TEXT_X_NAME = 150;
 
 window.renderStatistics = function(ctx, names, times) {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
@@ -12,44 +11,35 @@ window.renderStatistics = function(ctx, names, times) {
     ctx.fillStyle = 'white';
     ctx.fillRect(100, 10, CLOUSE_WIDTH, CLOUSE_HEIGHT);
     ctx.font = '16px PT Mono';
-    ctx.fillStyle = "black";
+    ctx.fillStyle = '#000000';
     ctx.fillText('Ура вы победили!', 120, 30);
     ctx.fillText('Список результатов:', 120, 50);
     var XColumn = 140;
     var criatYColumn = function(hieghtColumn) {
         return CLOUSE_HEIGHT - hieghtColumn - 20;
-    }
-    var criatXTextName = function(XText) {
-        return XText + WIDTH_COLUMN + DISTANCE;
-
-    }
+    };
     var criatXColumn = function(XColumn) {
         return XColumn + WIDTH_COLUMN + DISTANCE;
-    }
-
+    };
     var max = Math.round(times[0]);
     for (var i = 0; i < times.length; i++) {
         if (max < Math.round(times[i])) {
             max = Math.round(times[i]);
         }
-    }
-    var color = Math.random();
+    };
     var criatHeightColumn = function(max, timeColumnNumber) {
         return MAXHIEGHT_COLUMN * timeColumnNumber / max;
-    }
+    };
     for (var i = 0; i < times.length; i++) {
         if (names[i] === 'Вы') {
             ctx.fillStyle = 'rgba(255, 0, 0, 1)';
         } else {
             ctx.fillStyle = 'rgba(0,0,153,' + Math.random() + ')';
-        }
+        };
         ctx.fillRect(XColumn, criatYColumn(criatHeightColumn(max, Math.round(times[i]))), WIDTH_COLUMN, criatHeightColumn(max, Math.round(times[i])));
         ctx.fillStyle = 'black';
         ctx.fillText(names[i], XColumn, TEXT_Y_NAME);
         ctx.fillText(Math.round(times[i]), XColumn, criatYColumn(criatHeightColumn(max, Math.round(times[i]))) - 10);
         XColumn = criatXColumn(XColumn);
-
-    }
-
-
-}
+    };
+};
