6198342c1d927a86303f264e7613226329b143cc
Venera <venera_husainova@mail.ru>
2019-09-24 11:35:57 +0300

    работа над ошибками

--- js/stat.js
+++ js/stat.js
@@ -1,11 +1,21 @@
 'use strict';
+var CLOUSE_WIDTH = 420;
+var CLOUSE_HEIGHT = 270;
+var WIDTH_COLUMN = 40;
+var MAXHIEGHT_COLUMN = 150;
+var DISTANCE = 50;
+var TEXT_Y_NAME = 270;
+var criatYColumn = function (hieghtColumn) {
+  return CLOUSE_HEIGHT - hieghtColumn - 20;
+};
+var criatXColumn = function (xItem) {
+  return xItem + WIDTH_COLUMN + DISTANCE;
+};
+var criatHeightColumn = function (maxValue, timeColumnNumber) {
+  return MAXHIEGHT_COLUMN * timeColumnNumber / maxValue;
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  var CLOUSE_WIDTH = 420;
-  var CLOUSE_HEIGHT = 270;
-  var WIDTH_COLUMN = 40;
-  var MAXHIEGHT_COLUMN = 150;
-  var DISTANCE = 50;
-  var TEXT_Y_NAME = 270;
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, CLOUSE_WIDTH, CLOUSE_HEIGHT);
   ctx.fillStyle = 'white';
@@ -15,21 +25,14 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 30);
   ctx.fillText('Список результатов:', 120, 50);
   var XColumn = 140;
-  var criatYColumn = function (hieghtColumn) {
-    return CLOUSE_HEIGHT - hieghtColumn - 20;
-  };
-  var criatXColumn = function (xItem) {
-    return xItem + WIDTH_COLUMN + DISTANCE;
-  };
   var max = Math.round(times[0]);
+
   for (var i = 0; i < times.length; i++) {
     if (max < Math.round(times[i])) {
       max = Math.round(times[i]);
     }
   }
-  var criatHeightColumn = function (maxValue, timeColumnNumber) {
-    return MAXHIEGHT_COLUMN * timeColumnNumber / maxValue;
-  };
+
   for (var j = 0; j < times.length; j++) {
     if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
